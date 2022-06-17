a3bcdf967aaed87a3ec9e5798da92dc88b260d8c
Kei037 <juneway@hanmail.net>
2022-06-17 17:52:16 +0900

    2022-06-17 study
    study

--- eclipse-workspace/jy200/src/main/java/com/varxyz/jy200/mod008/AddArguments3.java
+++ eclipse-workspace/jy200/src/main/java/com/varxyz/jy200/mod008/AddArguments3.java
@@ -1,6 +1,12 @@
 package com.varxyz.jy200.mod008;
 
 public class AddArguments3 {
+	
+	public static int parseInt(String str) throws NumberFormatException {
+		int num = Integer.parseInt("25");
+		return num;
+	}
+	
 	public static void main(String[] args) {
 		int sum = 0;
 		for ( String arg : args ) {
